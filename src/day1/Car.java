b24da25079371ceba8fab38c37aeb6c9f8a648c0
Adit Shrestha <ashrestha1444@gmail.com>
2023-02-14 15:50:47 -0600

    Class and datatype

---
+++ src/day1/Car.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Car {
+    //State
+
+    // Color
+    // Name
+    // Size
+    // Engine
+    // Brakes
+
+
+    //Behavior
+
+    // Accelerating
+    // Braking
+    // Turing car on and off
+}
---
+++ src/day1/Laptop.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Laptop {
+    //State
+
+    // OS
+    // Memory
+    // Processor
+    // Storage
+    // Serial number
+    // Manufacture date
+    // Screen resolution
+
+
+    //Behavior
+
+    // Power ON
+    // Power OFF
+    // Current Battery Level
+    // Wifi Connection
+    // Bluetooth Connection
+
+}
